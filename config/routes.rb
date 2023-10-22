6cf0f2eba5e09e5ca416fefe5d6f09ff9f6595cd
Erick Rivera <yomellamoerick.771@gmail.com>
2023-10-22 03:42:32 -0300

    model and controller made

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'articles/index'
+      get 'articles/show'
+      get 'articles/create'
+      get 'articles/update'
+      get 'articles/destroy'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
